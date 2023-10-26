908114d592e9f3ff1b13fdf4865f1400c9d5698a
DTCurrie <devintcurrie@gmail.com>
2023-10-26 12:35:45 -0400

    Add sdk-svelte

---
+++ packages/sdk-svelte/.eslintrc.cjs
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  extends: ['@viamrobotics/eslint-config/svelte'],
+  parserOptions: {
+    project: ['./tsconfig.json'],
+  },
+};
---
+++ packages/sdk-svelte/svelte.config.js
@@ -0,0 +1,30 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+
+  kit: {
+    adapter: adapter(),
+
+    /*
+     * Merge our own includes with the generated includes from SvelteKit
+     * so that our JS config can be properly type-checked / linted
+     */
+    typescript: {
+      config: (tsconfig) => ({
+        ...tsconfig,
+        include: [
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          ...tsconfig.include,
+          '../.eslintrc.cjs',
+          '../.prettierrc.cjs',
+          '../svelte.config.js',
+        ],
+      }),
+    },
+  },
+};
+
+export default config;
